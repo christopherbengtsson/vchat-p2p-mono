860f6ce8544b179eb06d2dc96c1b6af4e9f421d9
Christopher Bengtsson <bengtsson.christopher@hotmail.com>
2024-10-22 22:31:34 +0200

    domain as nginx variable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Substitute environment variables in the nginx template
-envsubst '$SSL_CERTIFICATE $SSL_CERTIFICATE_KEY' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$SSL_CERTIFICATE $SSL_CERTIFICATE_KEY $FULL_DOMAIN' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Start Nginx in the foreground
 exec nginx -g 'daemon off;'
