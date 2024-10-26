e17de8af522860b33050fca53530b0db7bea4f1b
Christopher Bengtsson <bengtsson.christopher@hotmail.com>
2024-10-26 15:02:05 +0200

    try path without tailing slash

--- apps/server/src/socket/setupSocketServer.ts
+++ apps/server/src/socket/setupSocketServer.ts
@@ -20,7 +20,7 @@ import { setupWebRTC } from './webRtc.js';
 
 export function setupSocketServer(httpServer: Server, redisClient: Redis) {
   const io = new SocketServer(httpServer, {
-    path: '/ws/',
+    path: '/ws',
     adapter: createAdapter(redisClient),
     cors: {
       origin: (process.env.CORS_ORIGINS ?? '').split(','),
