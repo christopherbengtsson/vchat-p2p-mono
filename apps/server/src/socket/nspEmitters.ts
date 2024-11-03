e6ea7e3992bca86383f5e4f6d54485b7fa012c3b
Christopher Bengtsson <bengtsson.christopher@hotmail.com>
2024-11-03 15:16:34 +0100

    throttle connections-count from server

--- apps/server/src/socket/nspEmitters.ts
+++ apps/server/src/socket/nspEmitters.ts
@@ -3,9 +3,16 @@ import type {
   ServerToClientEvents,
 } from '@mono/common-dto';
 import type { Namespace } from 'socket.io';
+import throttle from 'lodash.throttle';
 
 export const nspEmitters = (
   nsp: Namespace<ClientToServerEvents, ServerToClientEvents>,
 ) => ({
-  connectionsCount: () => nsp.emit('connections-count', nsp.sockets.size),
+  connectionsCount: throttle(
+    () => {
+      nsp.emit('connections-count', nsp.sockets.size);
+    },
+    5000,
+    { leading: true, trailing: true },
+  ),
 });
