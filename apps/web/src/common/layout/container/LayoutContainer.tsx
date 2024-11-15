120b11e640e941051a3181960def1e357ad7801e
Christopher Bengtsson <bengtsson.christopher@hotmail.com>
2024-11-15 21:10:41 +0100

    remove background video from layout

--- apps/web/src/common/layout/container/LayoutContainer.tsx
+++ apps/web/src/common/layout/container/LayoutContainer.tsx
@@ -1,13 +1,10 @@
 import { Outlet } from 'react-router-dom';
-import { FullScreenVideoContainer } from './FullScreenVideoContainer';
 import { DialogsContainer } from './DialogsContainer';
 
 export function LayoutContainer() {
   return (
-    <div className="themes-wrapper relative w-full h-screen overflow-hidden">
-      <FullScreenVideoContainer />
-
-      <main className="relative z-10 bg-transparent w-full h-screen flex items-center justify-center p-4">
+    <div className="relative w-full h-screen bg-black overflow-hidden">
+      <main className="relative w-full h-screen flex items-center justify-center">
         <Outlet />
 
         <DialogsContainer />
