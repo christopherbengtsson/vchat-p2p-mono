bf9c5c2e3e9434bf9a377044c68a3741a40af6bc
Christopher Bengtsson <bengtsson.christopher@hotmail.com>
2025-02-08 16:15:57 +0100

    /e2e/failed-e2e-results

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -30,6 +30,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  /* Folder for test results */
+  outputDir: './e2e/failed-e2e-results',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
