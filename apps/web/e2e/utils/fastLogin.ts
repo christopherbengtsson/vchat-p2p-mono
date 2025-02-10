b18bf4c687d0ad1a655c77a6689d652594e534d4
Christopher Bengtsson <bengtsson.christopher@hotmail.com>
2025-02-10 23:39:10 +0100

    fix: e2e constent checkbox label

--- apps/web/e2e/utils/fastLogin.ts
+++ apps/web/e2e/utils/fastLogin.ts
@@ -2,7 +2,7 @@ import type { Page } from '@playwright/test';
 
 export const fastLogin = async (page: Page) => {
   await page
-    .getByRole('checkbox', { name: 'Accept terms and conditions' })
+    .getByRole('checkbox', { name: 'I confirm that I am 18 or older' })
     .check();
   await page.getByRole('button', { name: 'Fast login' }).click();
 };
