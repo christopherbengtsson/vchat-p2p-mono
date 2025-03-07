fdfe1fe107e44e7df58a0e6b5d2cc0a87596bba7
Christopher Bengtsson <bengtsson.christopher@hotmail.com>
2025-03-07 19:52:14 +0100

    fix incorrect env var for wdyr

--- apps/web/src/wdyr.ts
+++ apps/web/src/wdyr.ts
@@ -1,7 +1,7 @@
 /// <reference types="@welldone-software/why-did-you-render" />
-import React from 'react'; // This import worked.
+import React from 'react';
 
-if (import.meta.env.DEV && Boolean(import.meta.env.VITE_WDYR)) {
+if (import.meta.env.DEV && import.meta.env.VITE_WDYR_ENABLED === 'true') {
   const { default: wdyr } = await import(
     '@welldone-software/why-did-you-render'
   );
