7fe44727bd3952ee4ab3122440bc679630ad4d89
Christopher Bengtsson <bengtsson.christopher@hotmail.com>
2025-05-29 00:48:53 +0200

    controller base routes

--- apps/server/src/common/config/model/HttpRoute.ts
+++ apps/server/src/common/config/model/HttpRoute.ts
@@ -7,7 +7,7 @@ export enum HttpRoute {
 
 export const HttpRoutePaths: Record<HttpRoute, string> = {
   [HttpRoute.BASE]: HttpRoute.BASE,
-  [HttpRoute.HEALTH]: HttpRoute.HEALTH,
-  [HttpRoute.METRICS]: HttpRoute.METRICS,
+  [HttpRoute.HEALTH]: `${HttpRoute.BASE}${HttpRoute.HEALTH}`,
+  [HttpRoute.METRICS]: `${HttpRoute.BASE}${HttpRoute.METRICS}`,
   [HttpRoute.SIGNATURE]: `${HttpRoute.BASE}${HttpRoute.SIGNATURE}`,
 } as const;
