406db577a098f90abedfc05c049d1bf55c6dad53
Christopher Bengtsson <bengtsson.christopher@hotmail.com>
2025-06-19 18:48:12 +0200

    calculated matchmaking job interval

--- apps/server/src/feature/matchmaking/config/MatchmakingJobConfig.ts
+++ apps/server/src/feature/matchmaking/config/MatchmakingJobConfig.ts
@@ -6,11 +6,25 @@ import { MaintenanceJobEntry } from '../service/job/MaintenanceJobEntry.js';
 import { MonitoringJobEntry } from '../service/job/MonitoringJobEntry.js';
 import { MATCHMAKING_JOB } from '../model/MatchmakingJob.js';
 
+// TODO: Needs to be configurable
+// IO-bound workload with 4 workers on 2 vCPUs
+const MATCHMAKING_JOB_CONFIG = {
+  AVERAGE_PROCESSING_TIME_MS: 1600, // Average time to process a matchmaking job in milliseconds when running E2E tests locally
+  WORKER_COUNT: 4, // 2x number of CPUs, it's OK given the job is not CPU intensive but rather I/O bound (DB, Redis, etc.)
+  LOAD_FACTOR: 0.85, // Load factor to avoid overloading the system, a bit higher given the job is I/O bound
+} as const;
+
+// (1600 / 4) * 0.85 = 340ms
+const MATCHMAKING_INTERVAL =
+  (MATCHMAKING_JOB_CONFIG.AVERAGE_PROCESSING_TIME_MS /
+    MATCHMAKING_JOB_CONFIG.WORKER_COUNT) *
+  MATCHMAKING_JOB_CONFIG.LOAD_FACTOR;
+
 export const matchmakingConfig: QueueConfig[] = [
   {
     type: 'job',
     queueName: 'matchmaking',
-    workerCount: 4,
+    workerCount: MATCHMAKING_JOB_CONFIG.WORKER_COUNT,
     schedulers: [
       {
         schedulerId: 'matchmaking-processor',
@@ -18,7 +32,7 @@ export const matchmakingConfig: QueueConfig[] = [
           await MatchmakingJobEntry.create(job);
         },
         repeatOptions: {
-          every: 1000,
+          every: MATCHMAKING_INTERVAL,
         },
         jobTemplate: {
           name: MATCHMAKING_JOB.PROCESS_QUEUE,
